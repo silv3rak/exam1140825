920c5b85d3999c5653a85307ceb617e6d64ddd2f
silv3rak <shafyey637@gmail.com>
2025-11-16 10:26:31 +0330

    تغییرا

--- src/Notebook.php
+++ src/Notebook.php
@@ -9,23 +9,20 @@ require_once __DIR__ . '/Sellable.php';
  */
 class Notebook extends Product implements Sellable
 {
-    /**
-     * TODO(Task 4):
-     * Implement a constructor with the signature:
-     *
-     * public function __construct(int $id, string $title, int $price)
-     *
-     * It must assign the arguments to the protected properties.
-     */
+    public function __construct(int $id, string $title, int $price)
+{
+    $this->id = $id;
+    $this->title = $title;
+    $this->price = $price;
+}
 
-    /**
-     * TODO(Task 4):
-     * Implement getTypeLabel() so that it returns the exact Persian string "دفتر".
-     */
+public function getTypeLabel(): string
+{
+    return 'دفتر';
+}
+public function getFinalPrice(): int
+{
+    return $this->getPrice();
+}
 
-    /**
-     * TODO(Task 4):
-     * Implement getFinalPrice() so that it returns the original price without
-     * any discount.
-     */
 }
