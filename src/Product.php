aa4e6bba41431149edc394ef0ad9d9420d8cbdde
silv3rak <shafyey637@gmail.com>
2025-11-16 09:38:59 +0330

    Add Product properties

--- src/Product.php
+++ src/Product.php
@@ -10,40 +10,9 @@ declare(strict_types=1);
  */
 abstract class Product
 {
-    // TODO(Task 1):
-    // Add protected properties:
-    // - int $id
-    // - string $title
-    // - int $price
+protected int $id;
+protected string $title;
+protected int $price;
 
-    /**
-     * TODO(Task 1):
-     * Implement a constructor with the exact signature:
-     *
-     * public function __construct(int $id, string $title, int $price)
-     *
-     * It must assign the arguments to the protected properties.
-     */
-
-    /**
-     * TODO(Task 1):
-     * Implement this method to return the product title.
-     *
-     * public function getTitle(): string
-     */
-
-    /**
-     * TODO(Task 1):
-     * Implement this method to return the product price.
-     *
-     * public function getPrice(): int
-     */
-
-    /**
-     * Each subclass must return a human-readable type label,
-     * for example "Book" or "Notebook".
-     *
-     * This method MUST stay abstract.
-     */
     abstract public function getTypeLabel(): string;
 }
